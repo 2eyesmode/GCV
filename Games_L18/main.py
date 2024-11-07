777964c15e24c3c2b25317d4c2a6517cf2113a51
Dima Stepanishchev <2eyesmode@gmail.com>
2024-11-07 19:52:25 +0100

    UPD_1

--- Games_L18/main.py
+++ Games_L18/main.py
@@ -9,3 +9,23 @@ def main():
         print("6. Сапер")
         print("7. Выход")
         choice = input("Выберите игру (1-6): ")
+
+        if choice == "1":
+            guess_number()
+            print()
+        if choice == "2":
+            ssp()
+            print()
+        if choice == "3":
+            quiz()
+            print()
+        if choice == "4":
+            gallows()
+            print()
+        if choice == "5":
+            text_quest()
+            print()
+        if choice == "6":
+            sapper()
+            print()
+
